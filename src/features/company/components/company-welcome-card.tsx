77d661acd0b394ec73cb927c95b72cc0810aeda0
jekovniki <jekovniki95@gmail.com>
2025-05-12 09:05:15 +0300

    feat(welcome-card): update and rebuild welcome card

--- src/features/company/components/company-welcome-card.tsx
+++ src/features/company/components/company-welcome-card.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import welcomeImage from "../../../assets/welcome-icon.svg";
 import { useGetCompany } from "../api/use-get-company";
 import { Loader } from "@/components/loader";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const CompanyWelcomeCard = () => {
   const { t } = useTranslation();
@@ -13,34 +14,40 @@ export const CompanyWelcomeCard = () => {
   return (
     <div className="bg-welcome-card flex justify-between shadow-md">
       <div className="pl-4 flex flex-col justify-center">
-        {isLoading ? (
-          <div className="flex items-center justify-center px-10">
-            <Loader />
-          </div>
-        ) : (
-          <>
-            <h2 className="text-[#0c2134] text-[24px] font-thin mb-4">
-              {t("dashboard.welcomeCard.title")},{" "}
-              <strong className="font-bolder">
-                {sessionData?.firstName || ""}
-              </strong>
-              !
-            </h2>
-            <p className="font-thin mb-2">{t("dashboard.welcomeCard.text")}.</p>
-            <ul className="standard-ul">
-              <li className="font-thin">
-                <strong>{companyName}</strong>
-              </li>
-              <li className="font-thin">
+        <h2 className="text-[#0c2134] text-[24px] font-thin mb-4">
+          {t("dashboard.welcomeCard.title")},{" "}
+          <strong className="font-bolder">
+            {isLoading ? (
+              <Skeleton className="h-[20px] inline-block w-[100px] bg-secondary" />
+            ) : (
+              sessionData?.firstName || ""
+            )}
+          </strong>
+          !
+        </h2>
+        <p className="font-thin mb-2">{t("dashboard.welcomeCard.text")}.</p>
+        <ul className="standard-ul">
+          <li className="font-thin">
+            {isLoading ? (
+              <Skeleton className="h-[20px] inline-block w-[100px] bg-secondary" />
+            ) : (
+              <strong>{companyName}</strong>
+            )}
+          </li>
+          <li className="font-thin">
+            {isLoading ? (
+              <Skeleton className="h-[20px] inline-block w-[100px] bg-secondary" />
+            ) : (
+              <>
                 {t("dashboard.welcomeCard.subtext")}:{" "}
                 <strong>{companyUic}</strong>
-              </li>
-            </ul>
-            <p className="font-thin mt-6 italic">
-              {t("dashboard.welcomeCard.message")}
-            </p>
-          </>
-        )}
+              </>
+            )}
+          </li>
+        </ul>
+        <p className="font-thin mt-6 italic">
+          {t("dashboard.welcomeCard.message")}
+        </p>
       </div>
       <div className="pr-4 h-[386px] flex justify-end items-end">
         <img src={welcomeImage} alt={companyName} />
