b8eac1bffcd662d62d562ee1834b7b6a5f951ee1
jekovniki <jekovniki95@gmail.com>
2025-05-18 09:09:06 +0300

    feat(wallet-structure): add wallet structure

---
+++ src/features/wallets/containers/add-wallet-structure-modal.tsx
@@ -0,0 +1,94 @@
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
+interface AddWalletStructureModalProps {
+  toggleFormVisibility: () => void;
+}
+
+const AddWalletStructureModal = ({
+  toggleFormVisibility,
+}: AddWalletStructureModalProps) => {
+  const { t } = useTranslation();
+  const [view, setView] = useState<"overview" | "manual" | "email">("overview");
+
+  return (
+    <>
+      <div className="p-6 flex items-center justify-center">
+        {view === "overview" && (
+          <div className="py-6">
+            <p className="text-[16px] text-center text-[#0C2134BF]">
+              {t("dialog.wallet.add.overview.description")}
+            </p>
+          </div>
+        )}
+        {view === "email" && (
+          <div className="flex-col">
+            <p className="text-[16px] text-center text-[#0C2134BF] mb-4">
+              {t("dialog.wallet.add.email.description.partOne")}{" "}
+              <strong>{import.meta.env.VITE_WALLET_UPLOAD_EMAIL}</strong>
+            </p>
+            <p className="text-[16px] text-center text-[#0C2134BF] mb-4">
+              {t("dialog.wallet.add.email.description.partTwo")}
+            </p>
+            <p className="text-[16px] text-center text-[#0C2134BF]">
+              {t("dialog.wallet.add.email.description.final")}
+            </p>
+          </div>
+        )}
+        {view === "manual" && <div></div>}
+      </div>
+      <div className="border-t-[1px] p-6 flex items-center justify-between gap-4">
+        <Button
+          variant="secondary"
+          type="button"
+          onClick={toggleFormVisibility}
+        >
+          {t("dialog.wallet.add.buttons.cancel")}
+        </Button>
+        <div className="flex items-center gap-4">
+          {view === "overview" && (
+            <>
+              <Button onClick={() => setView("manual")}>
+                {t("dialog.wallet.add.overview.buttons.manual")}
+              </Button>
+              <Button onClick={() => setView("email")}>
+                {t("dialog.wallet.add.overview.buttons.automatic")}
+              </Button>
+            </>
+          )}
+          {view === "manual" && (
+            <>
+              <Button
+                variant="secondary"
+                type="button"
+                onClick={() => setView("overview")}
+              >
+                {t("dialog.wallet.add.email.goBack")}
+              </Button>
+              <Button onClick={() => setView("manual")}>
+                {t("dialog.wallet.add.overview.buttons.manual")}
+              </Button>
+            </>
+          )}
+          {view === "email" && (
+            <>
+              <Button
+                variant="secondary"
+                type="button"
+                onClick={() => setView("overview")}
+              >
+                {t("dialog.wallet.add.email.goBack")}
+              </Button>
+              <Button type="button" onClick={toggleFormVisibility}>
+                {t("dialog.wallet.add.email.button")}
+              </Button>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddWalletStructureModal;
--- src/features/wallets/containers/entity-wallet-structure.tsx
+++ src/features/wallets/containers/entity-wallet-structure.tsx
@@ -3,6 +3,16 @@ import { useGetWalletStructureBy } from "../api/use-get-wallet";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
+import { AccessVisibility } from "@/features/auth/components/access-visibility";
+import { UserPermission } from "@/features/auth/types/permissions";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import AddWalletStructureModal from "./add-wallet-structure-modal";
+import { useState } from "react";
 
 interface EntityWalletStructureProps {
   id: string;
@@ -11,11 +21,14 @@ interface EntityWalletStructureProps {
 const EntityWalletStructure = ({ id }: EntityWalletStructureProps) => {
   const { t } = useTranslation();
   const { data, isLoading } = useGetWalletStructureBy(id, "code");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const allAssets = data?.data?.assets;
   //   const assetOverview = data?.data?.overview;
-  console.log("id : ", id);
-  console.log("allAssets : ", allAssets);
-  console.log("allAssets.length : ", allAssets?.length);
+
+  const handleFormVisibility = () => {
+    setModalIsOpen(!modalIsOpen);
+  };
+
   return (
     <div className="bg-white shadow-md hover:shadow-sm transition-all p-4">
       <h3 className="mb-4 font-light text-[13px] text-[#0C213473]">
@@ -62,7 +75,27 @@ const EntityWalletStructure = ({ id }: EntityWalletStructureProps) => {
                 <p className="text-[#0C2134BF] font-light text-[14px] mb-4">
                   {t("dashboard.assets.noAssets.description")}
                 </p>
-                <Button>{t("dashboard.assets.noAssets.button")}</Button>
+                <AccessVisibility
+                  accessLevelRequired={UserPermission.assetCreate}
+                >
+                  <Dialog open={modalIsOpen}>
+                    <DialogTrigger asChild>
+                      <Button onClick={handleFormVisibility}>
+                        {t("dashboard.assets.noAssets.button")}
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent className="wide-modal">
+                      <DialogTitle className="pt-6 pb-2 px-4 text-center">
+                        {t("dialog.wallet.add.title")}
+                      </DialogTitle>
+                      <div className="bg-white border-t-[1px] md:rounded-b">
+                        <AddWalletStructureModal
+                          toggleFormVisibility={handleFormVisibility}
+                        />
+                      </div>
+                    </DialogContent>
+                  </Dialog>
+                </AccessVisibility>
               </div>
             )}
           </>
