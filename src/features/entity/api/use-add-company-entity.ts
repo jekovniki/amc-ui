689d15f198f20e31967d4e24f1084984dd813734
jekovniki <jekovniki95@gmail.com>
2025-06-17 17:16:32 +0300

    feat(403): fix 403 error when adding new company

--- src/features/entity/api/use-add-company-entity.ts
+++ src/features/entity/api/use-add-company-entity.ts
@@ -6,15 +6,18 @@ import {
 } from "@tanstack/react-query";
 import { AddEntityRequest, Entity } from "../types/entity";
 import { EntityQueries } from "./query-keys";
+import { refreshTokenAndRetry } from "@/features/auth/services/refresh-token-error-handler";
 
 export function useAddCompanyEntity() {
   const client = useQueryClient();
 
   return useMutation<{ data: Entity }, Error, AddEntityRequest>({
     mutationFn: (input: AddEntityRequest) => api.post("/api/v1/entity/", input),
-    onSuccess: () =>
+    onSuccess: () => {
+      refreshTokenAndRetry(); // necessary for Backend
       client.invalidateQueries(
         EntityQueries.Entities as InvalidateQueryFilters
-      ),
+      );
+    },
   });
 }
