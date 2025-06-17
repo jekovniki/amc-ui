feacb1c0d883c98899b2162a5bc9003001793a6f
jekovniki <jekovniki95@gmail.com>
2025-06-17 09:30:21 +0300

    feat(use-get-file): update with folder name

--- src/features/file/api/use-get-file-url-by-filename.ts
+++ src/features/file/api/use-get-file-url-by-filename.ts
@@ -4,7 +4,7 @@ import api from "@/lib/fetch";
 export function useGetFileURLByFilename(fileName: string, enabled: boolean) {
   return useQuery<{ data: string }>({
     queryKey: [],
-    queryFn: () => api.get(`/api/v1/file/${fileName}`),
+    queryFn: () => api.get(`/api/v1/file/${fileName}?folder=logos`),
     enabled: enabled && !!fileName,
   });
 }
